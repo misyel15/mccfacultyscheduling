06a459c44918b92e9dbe2279c5b3fe114f828c44
Faculty Loading <cosemichelle2@gmail.com>
2024-10-04 10:05:43 +0800

    Update timeslot.php

--- admin/timeslot.php
+++ admin/timeslot.php
@@ -156,7 +156,7 @@ $('#manage-timeslot').submit(function(e) {
     }
     
     $.ajax({
-        url: 'ajax.php?action=save_timeslot',
+        url: 'url: 'https://mccfacultyschedulin.com/admin/ajax.php?action=save_timeslot',
         data: new FormData($(this)[0]),
         cache: false,
         contentType: false,
