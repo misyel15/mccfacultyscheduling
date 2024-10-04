a1c160a7a50ffb59aab183b5b81963c009023d6d
Faculty Loading <cosemichelle2@gmail.com>
2024-10-04 08:41:55 +0800

    Update db_connect.php

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,11 +1,12 @@
 <?php
 // Define the database connection parameters
-$MySQL_username = 'u510162695_scheduling_db';
-$Password = '1Scheduling_db';
-$MySQL_database_name = 'u510162695_scheduling_db';
+$host = '3307';
+$username = 'u510162695_scheduling_db';
+$password = '1Scheduling_db';
+$database = 'u510162695_scheduling_db';
 
 // Create a new MySQLi object
-$conn = new mysqli('localhost', $MySQL_username, $Password, $MySQL_database_name);
+$conn = new mysqli($host, $username, $password, $database);
 
 // Check if the connection was successful
 if ($conn->connect_error) {
