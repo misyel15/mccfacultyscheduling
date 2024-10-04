a4b9db1aa7a96c3faa90855ed0b79c57c9918368
misyel15 <cosemichelle2@gmail.com>
2024-10-04 14:04:03 +0800

    Update db_connect.php

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,4 +1,9 @@
 <?php
+// Enable error reporting
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Define the database connection parameters
 $MySQL_username = 'u510162695_scheduling_db';
 $Password = '1Scheduling_db';
@@ -10,4 +15,18 @@ $conn = new mysqli('localhost', $MySQL_username, $Password, $MySQL_database_name
 // Check if the connection was successful
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+} else {
+    // Test query
+    $result = $conn->query("SHOW TABLES");
+    if ($result) {
+        while ($row = $result->fetch_row()) {
+            echo $row[0] . "<br>";
+        }
+    } else {
+        echo "Error: " . $conn->error;
+    }
 }
+
+// Close the connection
+$conn->close();
+?>
