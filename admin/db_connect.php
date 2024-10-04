761802e3d572b1113220c0de085c8a4d4cf8902c
misyel15 <cosemichelle2@gmail.com>
2024-10-04 14:21:26 +0800

    Update db_connect.php

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL);
 
 // Change these values to match your hosting and database credentials
 $host = '127.0.0.1'; // Use the actual hostname (commonly 'localhost' or provided by your hosting service)
-$port = '3307'; // Port number, if it's not the default (3306)
+$port = '3306'; // Port number, if it's not the default (3306)
 $username = 'u510162695_scheduling_db';
 $password = '1Scheduling_db';
 $database = 'u510162695_scheduling_db';
