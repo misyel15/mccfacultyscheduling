5341bd28f2b8ee035d5ed5c4554e526e4608d5c4
Faculty Loading <cosemichelle2@gmail.com>
2024-10-04 08:46:20 +0800

    Update db_connect.php

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,12 +1,18 @@
 <?php
+// Enable error reporting
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Define the database connection parameters
-$host = '3307';
+
 $username = 'u510162695_scheduling_db';
 $password = '1Scheduling_db';
 $database = 'u510162695_scheduling_db';
+$port = 3307; // Specify port if needed
 
 // Create a new MySQLi object
-$conn = new mysqli($host, $username, $password, $database);
+$conn = new mysqli($host, $username, $password, $database, $port);
 
 // Check if the connection was successful
 if ($conn->connect_error) {
