fe8b949555502f1db2e5852f9876eac92dc24875
Faculty Loading <cosemichelle2@gmail.com>
2024-10-04 08:53:32 +0800

    Update courses.php

--- admin/courses.php
+++ admin/courses.php
@@ -21,6 +21,7 @@ if (!$dept_id) {
 
 <!-- Include SweetAlert JS -->
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
 <!-- Include DataTables JS -->
 <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
