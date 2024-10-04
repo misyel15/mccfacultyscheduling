89af6d68a06203f2f93b9fa1efc04c82df584894
misyel15 <cosemichelle2@gmail.com>
2024-10-04 14:13:16 +0800

    Update db_connect.php

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -9,7 +9,7 @@ $Password = '1Scheduling_db';
 $MySQL_database_name = 'u510162695_scheduling_db';
 
 // Create a new MySQLi object
-$conn = new mysqli('localhost', $MySQL_username, $Password, $MySQL_database_name);
+$conn = new mysqli($MySQL_username, $Password, $MySQL_database_name);
 
 // Check if the connection was successful
 if ($conn->connect_error) {
