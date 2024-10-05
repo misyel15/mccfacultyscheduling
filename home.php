9c42088859b4caee29859c4d3b17e3ff3dc9d609
misyel15 <cosemichelle2@gmail.com>
2024-10-05 12:59:33 +0800

    Update home.php

--- home.php
+++ home.php
@@ -5,37 +5,39 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
   <title>Mcc Faculty Scheduling</title>
-  <link rel="icon" href="back.png" type="image/png">
+  <link rel="icon" href="back.png" type="image/png">      
 
   <!-- Include Bootstrap for styling -->
   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
   <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
-    <link href="vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
-    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+  <link href="vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
+  <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
 
   <?php include('./header.php'); ?>
 
   <?php 
-  if(isset($_SESSION['login_id']))
-  header("location:index.php");
+  if(isset($_SESSION['login_id'])) {
+      header("location:index.php");
+      exit(); // It's a good practice to exit after a header redirect
+  }
   ?>
 
 </head>
+
 <style>
-  body{
+  body {
     width: 100%;
     height: calc(100%);
-    position:fixed;
+    position: fixed;
   }
-  #main{
+  #main {
     width: calc(100%);
     height: calc(100%);
-    display:flex;
-    align-items:center;
-    justify-content:center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
-  #login{
-  }
+  #login { }
   
   .navbar-brand {
     color: white;
@@ -49,12 +51,9 @@
 
   <!-- Navigation Bar -->
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">
-  <img src="back.png" alt="Logo" style="width: 40px; height: 30px; margin-right: 10px;"> Faculty Scheduling System
-</a>
-
-
-   
+    <a class="navbar-brand" href="#">
+      <img src="back.png" alt="Logo" style="width: 40px; height: 30px; margin-right: 10px;"> Faculty Scheduling System
+    </a>
   </nav>
 
   <main id="main" class="bg-dark">
@@ -65,9 +64,11 @@
             <h4><b>Welcome To Faculty Scheduling System</b></h4>
             <div class="form-group">
               <label for="id_no" class="control-label">Please enter your Faculty ID No.</label>
-              <input type="text" id="id_no" name="id_no" class="form-control">
+              <input type="text" id="id_no" name="id_no" class="form-control" required>
             </div>
-            <center><button class="btn-sm btn-block btn-wave col-md-4 btn-primary">Login</button></center>
+            <center>
+              <button type="submit" class="btn-sm btn-block btn-wave col-md-4 btn-primary">Login</button>
+            </center>
           </form>
           <br>
         </div>
@@ -83,28 +84,33 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
   <script>
-    $('#login-form').submit(function(e){
+    $('#login-form').submit(function(e) {
       e.preventDefault();
-      $('#login-form button[type="button"]').attr('disabled',true).html('Logging in...');
-      if($(this).find('.alert-danger').length > 0 )
+      const $submitButton = $(this).find('button[type="submit"]');
+      $submitButton.attr('disabled', true).html('Logging in...');
+      
+      // Remove previous error message if exists
+      if ($(this).find('.alert-danger').length > 0) {
         $(this).find('.alert-danger').remove();
+      }
+      
       $.ajax({
-        url:'admin/ajax.php?action=login_faculty',
-        method:'POST',
-        data:$(this).serialize(),
-        error:err=>{
+        url: 'admin/ajax.php?action=login_faculty',
+        method: 'POST',
+        data: $(this).serialize(),
+        error: err => {
           console.log(err);
-          $('#login-form button[type="button"]').removeAttr('disabled').html('Login');
+          $submitButton.removeAttr('disabled').html('Login');
         },
-        success:function(resp){
-          if(resp == 1){
-            location.href ='index.php';
-          }else{
+        success: function(resp) {
+          if (resp == 1) {
+            location.href = 'index.php';
+          } else {
             $('#login-form').prepend('<div class="alert alert-danger">ID Number is incorrect.</div>');
-            $('#login-form button[type="button"]').removeAttr('disabled').html('Login');
+            $submitButton.removeAttr('disabled').html('Login');
           }
         }
-      })
+      });
     });
   </script>
 
