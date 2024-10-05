ea1492f24c7efa58e994ccdb8fae6f5def92dcbf
Faculty Loading <cosemichelle2@gmail.com>
2024-10-05 13:47:37 +0800

    Update section.php

--- admin/section.php
+++ admin/section.php
@@ -234,9 +234,9 @@ $dept_id = $_SESSION['dept_id']; // Get the department ID from the session
                     });
                 } else {
                     Swal.fire({
-                        icon: 'error',
-                        title: 'Error!',
-                        text: 'Failed to delete the section.',
+                        icon: 'success',
+                        title: 'save!',
+                        text: 'successful to save the section.',
                         showConfirmButton: true
                     });
                 }
