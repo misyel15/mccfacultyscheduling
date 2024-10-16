22c574c6d3c2639aa6da237b9d7cce62c3767929
Faculty Loading <cosemichelle2@gmail.com>
2024-10-16 14:12:16 +0800

    Update header.php

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -13,7 +13,7 @@ if (!isset($_SESSION['username']) || !isset($_SESSION['dept_id'])) {
 <head>
     <!-- Required meta tags-->
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="au theme template">
     <meta name="author" content="Hau Nguyen">
     <meta name="keywords" content="au theme template">
