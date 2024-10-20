8ef81a7afb74d7fa417f914bd1fec79eac61ae06
Faculty Loading <cosemichelle2@gmail.com>
2024-10-21 00:49:56 +0800

    Update section.php

--- admin/section.php
+++ admin/section.php
@@ -91,8 +91,7 @@ $dept_id = $_SESSION['dept_id']; // Get the department ID from the session
                     <div class="card-header">
                         <b>Section List</b>
                     <span class="">
-                       <button class="btn float-right btn-secondary" type="button" data-dismiss="modal">Close</button>
-
+                       <button> class="btn btn-primary btn-sm float-right" data-toggle="modal" data-target="#courseModal"><i class="fa fa-user-plus"></i> New Entry</button>
                         </span>
                     </div>
                     <div class="card-body">
